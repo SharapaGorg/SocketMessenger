8b7f64b6b7b70d2f0ebbdf89989ff54be8ebd08e
SharapaGorg <sharapov.savely@yandex.ru>
2022-04-07 15:16:40 +0300

    updated dist

--- client.py
+++ client.py
@@ -1,91 +1,91 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+try:
+    import asyncio
+    import socket
+    import sys
+    import threading
+    import utils.logger as logger
+    from prompt_toolkit.shortcuts import PromptSession
+    from prompt_toolkit.patch_stdout import patch_stdout
+    from prompt_toolkit import ANSI, print_formatted_text
 
-import asyncio
-import socket
-import sys
-import threading
-import utils.logger as logger
-from prompt_toolkit.shortcuts import PromptSession
-from prompt_toolkit.patch_stdout import patch_stdout
-from prompt_toolkit import ANSI, print_formatted_text
+    from rich.console import Console
+    from rich.text import Text
+    from rich.panel import Panel
 
-from rich.console import Console
-from rich.text import Text
-from rich.panel import Panel
+    from utils.vars import font_colors
 
-from utils.vars import font_colors
+    sock = socket.socket()
 
-sock = socket.socket()
+    # dev
+    #sock.connect(('localhost', 9090))
+    # prod
+    sock.connect(('185.127.224.67', 9002))
 
-# dev
-sock.connect(('localhost', 9090))
-# prod
-#sock.connect(('185.127.224.67', 9002))
-
-console = Console()
+    console = Console()
 
 
-def receive_data(conn):
-    try:
+    def receive_data(conn):
+        try:
+            while True:
+                _data = conn.recv(1024)
+                data = _data.decode('utf-8').split()
+                color, author, message = data[0], data[1], ' '.join(data[2:])
+
+                if author == 'HOST':
+                    content = Panel(Text(message, justify='center'))
+                    console.print(content)
+                else:
+                    content = ANSI(font_colors.get(color) + author + ' ' + message + font_colors.get('white'))
+                    print_formatted_text(content)
+
+        except:
+            pass
+
+    receive_thread_ = threading.Thread(target=receive_data, args=[
+        sock], name='Socket-Data-Receiver 1', daemon=True)
+
+
+    async def receiver():
+        try:
+            receive_thread_.start()
+
+        except asyncio.CancelledError:
+            logger.warning("Background receive task cancelled.")
+
+
+    async def typing():
+        session = PromptSession("[You]: ")
+
         while True:
-            _data = conn.recv(1024)
-            data = _data.decode('utf-8').split()
-            color, author, message = data[0], data[1], ' '.join(data[2:])
+            try:
+                message = await session.prompt_async()
 
-            if author == 'HOST':
-                content = Panel(Text(message, justify='center'))
-                console.print(content)
-            else:
-                content = ANSI(font_colors.get(color) + author + ' ' + message + font_colors.get('white'))
-                print_formatted_text(content)
+                sock.send(message.encode('utf-8'))
 
-    except:
-        # logger.warning('Client aborted')
-        pass
+                if message == 'exit':
+                    break
+
+            except (EOFError, KeyboardInterrupt):
+                return
 
 
-receive_thread_ = threading.Thread(target=receive_data, args=[
-    sock], name='Socket-Data-Receiver 1', daemon=True)
+    async def main():
+        with patch_stdout():
+            background_receiver = asyncio.create_task(receiver())
+
+            try:
+                await typing()
+            finally:
+                background_receiver.cancel()
+
+            print('Quitting chatting. Bye')
+
+            sock.close()
+            sys.exit()
 
 
-async def receiver():
-    try:
-        receive_thread_.start()
-
-    except asyncio.CancelledError:
-        logger.warning("Background receive task cancelled.")
-
-
-async def typing():
-    session = PromptSession("[You]: ")
-
-    while True:
-        try:
-            message = await session.prompt_async()
-
-            sock.send(message.encode('utf-8'))
-
-            if message == 'exit':
-                break
-
-        except (EOFError, KeyboardInterrupt):
-            return
-
-
-async def main():
-    with patch_stdout():
-        background_receiver = asyncio.create_task(receiver())
-
-        try:
-            await typing()
-        finally:
-            background_receiver.cancel()
-
-        print('Quitting chatting. Bye')
-
-        sock.close()
-        sys.exit()
-
-
-asyncio.run(main())
+    asyncio.run(main())
+except Exception as e:
+    logger.error(e)
